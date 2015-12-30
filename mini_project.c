40932115a64ba5ccb26abdbdbd207bb265b92cd5
oumayaSaggay <oumaya.23o@gmail.com>
2015-12-30 16:11:08 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,15 +1,14 @@
 void main()  {
-  // Software I2C connections
-sbit Soft_I2C_Scl           at RC3_bit;
-sbit Soft_I2C_Sda           at RC4_bit;
-sbit Soft_I2C_Scl_Direction at TRISC3_bit;
-sbit Soft_I2C_Sda_Direction at TRISC4_bit;
-// End Software I2C connections
+  ansel = anselh = portc = trisc=0;
+                  I2C1_Init(100000);
+                   I2C1_Start();
+                   sspstat=0b10010111;
+                   sspcon=0b10101000;
+                   sspbuf=0b01111111;
+                   sspcon2=0b00000001;
+                         I2C1_Wr(x);
+                          I2C1_Wr(y);
+                          portc1=I2C1_rd(0x30);
+                          I2C_stop();
 
-Soft_I2C_Start();
-Soft_I2C_Init();
-    error = Soft_I2C_Write(x,y_);
-error = Soft_I2C_Write(0x30);
 }
-
-
