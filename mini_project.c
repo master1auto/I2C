9367c0bc4c60225a823bfa79c17cdc4985ac7fce
oumayaSaggay <oumaya.23o@gmail.com>
2016-01-04 21:30:07 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,14 +1,16 @@
-void main()  {
-  ansel = anselh = portc = trisc=0;
-                  I2C1_Init(100000);
-                   I2C1_Start();
-                   sspstat=0b10010111;
-                   sspcon=0b10101000;
-                   sspbuf=0b01111111;
-                   sspcon2=0b00000001;
-                         I2C1_Wr(x);
-                          I2C1_Wr(y);
-                          portc1=I2C1_rd(0x30);
-                          I2C_stop();
+void main() {
 
+sspstat=0b10010111; 
+sspcon=0b10101000;
+sspbuf=0b00110000;   //l'adress 0x30
+sspcon2=0b00000001;
+sspif=0b00001000;   
+sspbuf=0b00010000;  //valeur de x=10d
+sspcon2=0b00000001;
+
+sspbuf=0b00110000;   //l'adress 0x30
+sspcon2=0b00000001;
+sspif=0b00001000;
+sspbuf=0b00010001; //valeur de y=11d
+sspcon2=0b00000001;
 }
