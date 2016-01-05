74d1566508a8e8695a771108460a9e287ed431b7
oumayaSaggay <oumaya.23o@gmail.com>
2016-01-05 11:19:46 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,16 +1,24 @@
-void main() {
 
-sspstat=0b10010111; 
-sspcon=0b10101000;
-sspbuf=0b00110000;   //l'adress 0x30
-sspcon2=0b00000001;
-sspif=0b00001000;   
-sspbuf=0b00010000;  //valeur de x=10d
-sspcon2=0b00000001;
 
-sspbuf=0b00110000;   //l'adress 0x30
+ void main() {
+
+sspstat=0b11000000;
+sspcon=0b00001000;
 sspcon2=0b00000001;
-sspif=0b00001000;
-sspbuf=0b00010001; //valeur de y=11d
+sspbuf=0b00110000;                   //l'adress 0x30
+pir1=0b10000000;
 sspcon2=0b00000001;
+sspbuf=0b00010000;                     //valeur de x=10d
+  while(sspif !=1)
+
+
+sspcon2=0b00000001;
+sspbuf=0b00110000;                   //l'adress 0x30
+pir1=0b10000000;
+sspcon2=0b00000001;
+sspbuf=0b00010001;                     //valeur de y=11d
+  while(sspif !=1)
+
+
+}
 }
