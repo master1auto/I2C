a5aa6dadf1af2838b2a0ef0c7b31803eab9644e0
oumayaSaggay <oumaya.23o@gmail.com>
2016-01-15 22:00:29 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -5,7 +5,8 @@
 sspstat=0b11000000;
 sspcon=0b00001000;
 sspcon2=0b00000001;
-sspbuf=0b00110000;                   //l'adress 0x30
+sspbuf=0b00110000;//l'adress 0x30
+while(BF);
 pir1=0b10000000;
 sspcon2=0b00000001;
 sspbuf=0b00010000;                     //valeur de x=10d
@@ -14,6 +15,7 @@ sspbuf=0b00010000;                     //valeur de x=10d
 
 sspcon2=0b00000001;
 sspbuf=0b00110000;                   //l'adress 0x30
+while(BF);
 pir1=0b10000000;
 sspcon2=0b00000001;
 sspbuf=0b00010001;                     //valeur de y=11d
